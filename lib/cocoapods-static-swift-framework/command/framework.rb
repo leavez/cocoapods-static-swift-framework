97cc1c0c8de638495e970e7f1170f3cec3cedc57
leavez <gaojiji@gmail.com>
2018-03-28 23:17:01 +0800

    update for cocoapods 1.5

--- lib/cocoapods-static-swift-framework/command/framework.rb
+++ lib/cocoapods-static-swift-framework/command/framework.rb
@@ -2,7 +2,6 @@ module Pod
     class Podfile
         def static_frameworks
             require 'cocoapods-static-swift-framework/command/patch/static_pod.rb'
-            require 'cocoapods-static-swift-framework/command/patch/add_generated_objc_header.rb'
         end
     end
 end
--- lib/cocoapods-static-swift-framework/gem_version.rb
+++ lib/cocoapods-static-swift-framework/gem_version.rb
@@ -1,3 +1,3 @@
 module CocoapodsStaticSwiftFramework
-  VERSION = "0.0.2"
+  VERSION = "0.3"
 end
