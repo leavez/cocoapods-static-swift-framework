55c615c15361f0288c056b73dc9ec9ca70dbd5d1
leavez <gaojiji@gmail.com>
2018-07-03 17:46:18 +0800

    add check

--- lib/cocoapods-static-swift-framework/main.rb
+++ lib/cocoapods-static-swift-framework/main.rb
@@ -1,4 +1,15 @@
 
 Pod::HooksManager.register('cocoapods-static-swift-framework', :pre_install) do |installer_context|
+
+    # check user_framework is on
+    podfile = installer_context.podfile
+    podfile.target_definition_list.each do |target_definition|
+        next if target_definition.name == "Pods"
+        if not target_definition.uses_frameworks?
+            STDERR.puts "[!] Cocoapods-static-swift-framework requires `use_frameworks!`".red
+            exit
+        end
+    end
+
     require_relative 'patch/static_pod'
 end
