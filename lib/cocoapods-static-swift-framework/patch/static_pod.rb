ea5d7120798884ec92bbf353001a89b3a97320e2
leavez <gaojiji@gmail.com>
2019-07-02 01:10:07 +0800

    support 1.6 and 1.7 simultaneously

--- lib/cocoapods-static-swift-framework/patch/static_pod.rb
+++ lib/cocoapods-static-swift-framework/patch/static_pod.rb
@@ -1,9 +1,26 @@
-module Pod
-    class Target
-        class BuildType
-            def linkage
-                :static
+
+if Gem::Version.new(Pod::VERSION) < Gem::Version.new('1.7')
+
+    module Pod
+        class PodTarget
+            def static_framework?
+                return true
             end
         end
     end
+
+else
+
+    # cocoapods 1.7 change the implementation of :static_framework?
+    # The 'BuildType' contain all related things.
+    module Pod
+        class Target
+            class BuildType
+                def linkage
+                    :static
+                end
+            end
+        end
+    end
+
 end
